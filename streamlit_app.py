65e440e0a42b1e783362b49b232576f4915064c1
RevathyDevadoss100 <142204241+RevathyDevadoss100@users.noreply.github.com>
2023-08-14 18:51:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Freerange egg')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
