92e558c733f4da9868c1ce1b2d87bc043844d372
RevathyDevadoss100 <142204241+RevathyDevadoss100@users.noreply.github.com>
2023-08-14 13:02:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-Streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My Parents New Healthy Dinner')
 
-Streamlit.header('Breakfast Menu')
-Streamlit.text('Omega 3 & Blueberry oatmeal')
-Streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Freerange egg')
